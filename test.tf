eb1e60d3bf67fab6ddf8ffcb5a09bd0d15337ba0
benarjee21 <kanuribenarjee@gmail.com>
2021-11-13 16:21:51 +0530

    tf fiix

--- test.tf
+++ test.tf
@@ -2,10 +2,9 @@ provider "aws" {
   region = "ap-south-1"
   access_key = "AKIAT5YOA2WDADWITQMU"
   secret_key = "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"
-
 }
 
 resource "aws_instance" "my_first_ec2" {
-    instance_type = "t2.micro"
     ami = "ami-0f1fb91a596abf28d"
+    instance_type = "t2.micro"
 }
